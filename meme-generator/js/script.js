fb2d9c16e86ccfa4f0445d895b908c970721a575
Joanne <joannesayswu@gmail.com>
2019-05-17 14:16:57 -0700

    add js to store user inputted values
    also add js folder & script and script src in html

---
+++ meme-generator/js/script.js
@@ -0,0 +1,5 @@
+var imageURL = document.getElementById('imageURL').value;
+var topText = document.getElementById('topText').value;
+var bottomText = document.getElementById('bottomText').value;
+
+alert("imageURL is: ", imageURL);
