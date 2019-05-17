5f213ce2cc2b22ac28a2deab7ae015b377fb34f4
Joanne <joannesayswu@gmail.com>
2019-05-17 15:40:43 -0700

    testing to see if multiple functions can be called on button click

--- meme-generator/js/script.js
+++ meme-generator/js/script.js
@@ -3,7 +3,6 @@ function getData() {
     var imageURL = document.getElementById('imageURL').value;
     var topText = document.getElementById('topText').value;
     var bottomText = document.getElementById('bottomText').value;
-    var message = "user entered this topText: " + topText;
     alert(message)
 }
 
