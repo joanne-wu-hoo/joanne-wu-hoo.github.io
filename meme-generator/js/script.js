5719eb8bf571572ad67af4c36fb5fb813daef884
Joanne <joannesayswu@gmail.com>
2019-05-17 15:57:51 -0700

    add functions to get user data and post meme, move button outside of form

--- meme-generator/js/script.js
+++ meme-generator/js/script.js
@@ -1,13 +1,16 @@
-
-function getData() {
-    var imageURL = document.getElementById('imageURL').value;
-    var topText = document.getElementById('topText').value;
-    var bottomText = document.getElementById('bottomText').value;
-    alert(message)
-}
-
 function addImage(imageURL) {
     var img = new Image();
     img.src = imageURL
     img_home.appendChild(img);
 }
+
+function getMeme(){
+    // Get user inputted data
+    var imageURL = document.getElementById('imageURL').value;
+    var topText = document.getElementById('topText').value;
+    var bottomText = document.getElementById('bottomText').value;
+    
+    // Call addImage function to post image
+    addImage(imageURL);    
+}
+
